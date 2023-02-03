63d3c3169ddc4758e314ff3694c55334eabe5c33
Yulia Luneva <whatswrongnow@mail.ru>
2023-02-04 01:16:21 +0300

    Add some begin screen. hope it work

--- app/src/main/java/com/example/begin/Adventure.kt
+++ app/src/main/java/com/example/begin/Adventure.kt
@@ -1,17 +1,18 @@
 package com.example.begin
 
+import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import com.example.begin.databinding.ActivityAdventureBinding
 
 class Adventure : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_adventure)
+    }
+
+    fun back(view: View){
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/example/begin/MainActivity.kt
+++ app/src/main/java/com/example/begin/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.begin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun toAdventure(view: View){
+        val intent = Intent(this, Adventure::class.java)
+        startActivity(intent)
+    }
+
+    fun toDnDBook(view: View){
+        val intent = Intent(this, DnDBook::class.java)
+        startActivity(intent)
+    }
+
+    fun toSettings(view: View){
+        val intent = Intent(this@MainActivity, Settings::class.java)
+        startActivity(intent)
     }
 }
